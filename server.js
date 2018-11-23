f75d182daa8927e89f2b67b8d9e28b90e29e5a78
Huy Tran <huy23tran11@gmail.com>
2018-11-22 23:12:39 -0800

    debugging

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ app.put('/image',(req, res)=> image.handleImage(req, res, db))
 app.post('/imageurl', (req, res)=> image.handleAPICall(req, res))
 
 app.listen(process.env.PORT || 3000,() =>{
-	console.log(`app is running on port 3000 ${rocess.env.PORT }`)
+	console.log(`app is running on port 3000 ${process.env.PORT }`)
 })
